9640faf167d64bc2dcc0c062f5f6458b95dd8fbe
Le Nam <nambluehc@gmail.com>
2023-02-13 00:39:30 +0700

    upload2

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ const btnLogin = document.querySelector('.btn-login')
 const modalBlock = document.querySelector('.modal-block')
 const modalBtnLogin = document.querySelector('.modal-btn-login')
 const login = document.querySelector('.login')
+let accountAPI = 'https://r6vol.wiremockapi.cloud/json/account'
+let accounts //store data of account
+
+async function getAccounts() {
+    try {
+        const response = await fetch(accountAPI);
+        accounts = await response.json();
+    } 
+    catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
 
 
 function showModal(){
@@ -12,20 +25,20 @@ function hideModal(){
     modal.classList.remove('open')
 }
 
-function hideModalAndChangeLogin(){
-    modal.classList.remove('open')
-    login.removeChild(login.firstChild)
-    login.innerHTML = `
-        <a href="/info.html">
-            <div class="name-student">
-                <p>Xin chào, Lê Thanh Nam</p>
-            </div>
-            <div class="avt-student" href="/info.html">
-                <img src="imgs/avatar-370-456322.webp" alt="">
-            </div>
-        </a>
-    `
-}
+// function hideModalAndChangeLogin(){
+//     modal.classList.remove('open')
+//     login.removeChild(login.firstChild)
+//     login.innerHTML = `
+//         <a href="/info.html">
+//             <div class="name-student">
+//                 <p>Xin chào, ${accounts[i].name}</p>
+//             </div>
+//             <div class="avt-student" href="/info.html">
+//                 <img src="imgs/avatar-370-456322.webp" alt="">
+//             </div>
+//         </a>
+//     `
+// }
 
 function checkLogin(){
     let code = document.querySelector('.input-mssv')
@@ -33,13 +46,38 @@ function checkLogin(){
     let wrongLogin = document.querySelector('.wrong-login')
     let input = document.querySelector('.input')
     
-    if (code.value == 20202020 && passWord.value == 123456) {
-        hideModalAndChangeLogin()
-    }
-    else{
+    getAccounts().then (()=> {
+        console.log(accounts)
+        for (let i = 0; i < accounts.length; i++) {
+            if(code.value == accounts[i].username && passWord.value == accounts[i].password ){
+                // hideModalAndChangeLogin()      
+                modal.classList.remove('open')
+                login.removeChild(login.firstChild)
+                login.innerHTML = `
+                    <a href="/info.html">
+                        <div class="name-student">
+                            <p>Xin chào, ${accounts[i].name}</p>
+                        </div>
+                        <div class="avt-student" href="/info.html">
+                            <img src="imgs/avatar-370-456322.webp" alt="">
+                        </div>
+                    </a>
+                `
+                break;
+            }
+        }    
         passWord.value = ""
         wrongLogin.innerHTML = '<p>x Sai tên đăng nhập hoặc mật khẩu !</p>'
-    }
+        postAccount()
+    })
+
+    // if (code.value == 20202020 && passWord.value == 123456) {
+    //     hideModalAndChangeLogin()
+    // }
+    // else{
+    //     passWord.value = ""
+    //     wrongLogin.innerHTML = '<p>x Sai tên đăng nhập hoặc mật khẩu !</p>'
+    // }
 }
 
 btnLogin.addEventListener('click', showModal)
@@ -63,3 +101,33 @@ setInterval(function() {
         currentIndex = (currentIndex + 1) % imgs.length;
         changeImage(currentIndex);
     }, 5000)
+
+
+
+
+
+// getAccounts().then (()=> {
+// console.log(accounts)})
+
+function postAccount(){
+    fetch(accountAPI, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        id: 5,
+        name: 'John Doe',
+        username: 'johndoe',
+        password: '123456',
+        mssv: 123456,
+        khoa: 'K66',
+        avt: '/imgs/b7f63ae99c6c95b6b5d4931f1a0ce4bf.jpg',
+        status: 0
+    })
+})
+  .then(response => response.json())
+  .then(jsonData => {
+    console.log('Success:', jsonData);
+  })
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+link web: https://quanlithuvien1992.000webhostapp.com/
+mssv 20202020 . mat khau: 123456
+
+link sourcecode: https://github.com/NomNom1992/quan_li_thu_vien
